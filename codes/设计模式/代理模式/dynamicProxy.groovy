06abc198234bee763caeaa1c10fd5b9bae6654bb
shen dongsheng <wideunique@qq.com>
2017-10-05 10:42:54 +0800

    动态代理模式

---
+++ codes/设计模式/代理模式/dynamicProxy.groovy
@@ -0,0 +1,34 @@
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+
+interface Subject{
+	void operation(String txt)
+}
+class ConcreteSubject implements Subject{
+	void operation(String txt){
+		println "ConcreteSubject operation:$txt"
+	}
+}
+class DynamicProxy implements InvocationHandler{
+	Subject realSubject
+	public Object invoke(Object target, Method method, Object[] args)
+	throws Throwable{
+		println "before:"+method.name
+		Object obj=null
+		if(args){
+			 obj=method.invoke(realSubject,args)
+		}else{
+			obj=method.invoke(realSubject)
+		}
+		println "after:"+args
+		return obj
+		
+	}
+	
+}
+Subject realSubject = new ConcreteSubject()
+InvocationHandler handler = new DynamicProxy(realSubject:realSubject)
+
+Subject s=(Subject)java.lang.reflect.Proxy.newProxyInstance(getClass().getClassLoader(), realSubject
+		.getClass().getInterfaces(), handler)
+s.operation("中国")
